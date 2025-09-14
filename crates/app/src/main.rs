1261144c8d6ea3dddd31d82d578602038fc23828
Arvin Morawej <arvin.morawej@gmail.com>
2025-09-14 15:20:28 -0400

    feat: Add Logging file

--- crates/app/src/main.rs
+++ crates/app/src/main.rs
@@ -1,12 +1,15 @@
 use game_core::prelude::*;
 use game_core::world::{Character, World};
+use std::fs::File;
 use tracing_subscriber::filter::LevelFilter;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     // Initialize tracing
     tracing_subscriber::fmt::fmt()
+        .with_ansi(false)
         .with_max_level(LevelFilter::DEBUG)
+        .with_writer(File::create("debug.log")?)
         .init();
 
     // Initialize world
