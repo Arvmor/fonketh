52a4a7821b69fec657df54eb5f131028158153f4
Arvin Morawej <arvin.morawej@gmail.com>
2025-09-13 21:42:02 -0400

    feat: Add logging into main app

--- crates/app/src/main.rs
+++ crates/app/src/main.rs
@@ -2,8 +2,12 @@ use game_core::prelude::*;
 use game_core::world::{Character, World};
 
 fn main() -> Result<()> {
+    // Initialize tracing
+    tracing_subscriber::fmt::init();
+
     let character = Character::new("John", 0);
     let world = World::new("1", vec![character]);
+    world.initialize()?;
 
     Ok(())
 }
