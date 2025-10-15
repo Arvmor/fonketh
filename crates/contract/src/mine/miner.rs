320478637ad5315a4e5f4e56809e3bfc28aa23d5
Arvin Morawej <arvin.morawej@gmail.com>
2025-10-15 13:59:26 -0400

    feat: cleanup miner module

--- crates/contract/src/mine/miner.rs
+++ crates/contract/src/mine/miner.rs
@@ -1,9 +1,5 @@
-use alloy::{
-    primitives::keccak256,
-    sol_types::{self, SolValue},
-};
-
 use crate::prelude::*;
+use alloy::{primitives::keccak256, sol_types::SolValue};
 
 /// Miner
 ///
@@ -11,7 +7,7 @@ use crate::prelude::*;
 #[derive(Debug)]
 pub struct Miner {
     pub(crate) factory: Address,
-    pub(crate) miner_address: Address,
+    pub(crate) address: Address,
     pub(crate) salt: U256,
     pub(crate) init_hash: B256,
     pub(crate) difficulty: Address,
@@ -21,7 +17,7 @@ impl Miner {
     /// Creates a new miner
     pub fn new(
         factory: Address,
-        miner_address: Address,
+        address: Address,
         salt: impl TryInto<U256>,
         init_hash: B256,
         difficulty: Address,
@@ -30,7 +26,7 @@ impl Miner {
 
         Self {
             factory,
-            miner_address,
+            address,
             salt,
             init_hash,
             difficulty,
@@ -40,7 +36,7 @@ impl Miner {
     /// Mines a new address
     pub fn mine(&mut self, nonce: U256, init_hash: B256) -> Option<U256> {
         // keccak256(abi.encodePacked(nonce, minerAddress));
-        let salt = keccak256((nonce, self.miner_address).abi_encode_packed());
+        let salt = keccak256((nonce, self.address).abi_encode_packed());
         let mined = self.factory.create2(salt, init_hash);
 
         // If passed the difficulty, return the nonce
@@ -58,10 +54,7 @@ impl Miner {
         self.salt += U256::ONE;
 
         // If mined, return the miner address and nonce
-        if let Some(nonce) = self.mine(self.salt, self.init_hash) {
-            return Some((self.miner_address, nonce));
-        }
-
-        None
+        self.mine(self.salt, self.init_hash)
+            .map(|n| (self.address, n))
     }
 }
