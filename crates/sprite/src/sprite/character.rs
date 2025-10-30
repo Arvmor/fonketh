7da627e717faeacd276a2da75ca09c2494aa31f0
Arvin Morawej <arvin.morawej@gmail.com>
2025-10-30 13:27:01 -0400

    refactor: Sprite crate modularized

---
+++ crates/sprite/src/sprite/character.rs
@@ -0,0 +1,11 @@
+use crate::sprite::Color;
+use image::Rgba;
+
+/// Hair Color
+pub const HAIR_COLOR: Color = Color(Rgba([0x79, 0x3d, 0x4e, 0xff]));
+/// Skin Color
+pub const SKIN_COLOR: Color = Color(Rgba([0xfb, 0x95, 0x85, 0xff]));
+/// Eyes Color
+pub const EYES_COLOR: Color = Color(Rgba([0x85, 0xa3, 0xc7, 0xff]));
+/// Clothing Color
+pub const CLOTHING_COLOR: Color = Color(Rgba([0x01, 0x76, 0x87, 0xff]));
---
+++ crates/sprite/src/sprite/mod.rs
@@ -0,0 +1,35 @@
+/// Player Character Specifications
+pub mod character;
+/// Sprite Image Module
+mod image;
+
+pub use image::{Color, SpriteImage};
+
+#[cfg(test)]
+mod tests {
+    use crate::sprite::character::HAIR_COLOR;
+    use crate::sprite::image::{Color, SpriteImage};
+    use std::collections::HashMap;
+
+    #[test]
+    fn load_sprite_sheet() -> anyhow::Result<()> {
+        SpriteImage::new("../../assets/textures/characters/gabe-idle-run.png")?;
+
+        Ok(())
+    }
+
+    #[test]
+    fn modify_color() -> anyhow::Result<()> {
+        let path = "../../assets/textures/characters/gabe.png";
+        let mut sprite_image = SpriteImage::new(path)?;
+
+        // Create a map of colors to modify
+        let map = HashMap::from([(HAIR_COLOR, Color::create(0x00, 0xc1, 0x9a, 0xff))]);
+
+        // Modify the color of the sprite image
+        sprite_image.modify_color(&map)?;
+        sprite_image.save(path)?;
+
+        Ok(())
+    }
+}
