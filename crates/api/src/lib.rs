84b652f85fe3ef2950b58d48b3a979d320868540
Arvin Morawej <arvin.morawej@gmail.com>
2025-11-13 15:26:22 -0500

    refactor: API endpoints and names

--- crates/api/src/lib.rs
+++ crates/api/src/lib.rs
@@ -1,31 +1,54 @@
-use actix_web::web::{self, ThinData};
-use actix_web::{App, HttpServer};
+use actix_web::Responder;
+use actix_web::web;
+use actix_web::{App, HttpServer, get};
 use game_primitives::WorldState;
+use serde::Serialize;
 
-use crate::mine::MineData;
+mod world_status;
 
-mod mine;
-
-pub struct ApiServer<W: WorldState> {
-    world: ThinData<MineData<W>>,
+/// Index route
+///
+/// Responds with a simple message
+#[get("/")]
+async fn index() -> impl Responder {
+    "Live Server"
 }
 
-impl<W: WorldState + 'static + Clone + Send + Sync> ApiServer<W> {
+/// Api Server
+///
+/// Responsible for running the API server
+pub struct ApiServer<W: WorldState> {
+    world: web::ThinData<W>,
+}
+
+impl<W, M, P> ApiServer<W>
+where
+    W: WorldState<Message = M, Player = P> + Clone + Send + Sync + 'static,
+    M: Serialize + 'static,
+    P: Serialize + 'static,
+{
+    /// Creates a new API server that holds resources
     pub fn new(world: W) -> Self {
-        let world = ThinData(MineData::new(world));
+        let world = web::ThinData(world);
         Self { world }
     }
 
+    /// Runs the API server
     pub async fn run(self) {
-        HttpServer::new(move || {
+        let app = move || {
             App::new()
                 .app_data(self.world.clone())
-                .service(web::resource("/mine").to(mine::mine::<W>))
-        })
-        .bind("0.0.0.0:8080")
-        .expect("Failed to bind to port 8080")
-        .run()
-        .await
-        .expect("Failed to run server");
+                .service(index)
+                .service(web::resource("/mine").to(world_status::mined_batch::<W>))
+                .service(web::resource("/players").to(world_status::players::<W, P>))
+                .service(web::resource("/chat").to(world_status::chat_messages::<W, M>))
+        };
+
+        HttpServer::new(app)
+            .bind("0.0.0.0:8080")
+            .expect("Failed to bind to port 8080")
+            .run()
+            .await
+            .expect("Failed to run server");
     }
 }
---
+++ crates/api/src/world_status.rs
@@ -0,0 +1,27 @@
+use actix_web::Responder;
+use actix_web::web;
+use game_primitives::WorldState;
+use serde::Serialize;
+
+/// Responds with latest state of the world.
+pub async fn mined_batch<W: WorldState>(data: web::ThinData<W>) -> impl Responder {
+    let batch = data.get_mining_batch();
+    web::Json(batch)
+}
+
+/// Responds with players in the world
+pub async fn players<W: WorldState<Player = P>, P: Serialize>(
+    data: web::ThinData<W>,
+) -> impl Responder {
+    let players = data.get_all_players().into_values().collect::<Vec<_>>();
+    web::Json(players)
+}
+
+/// Responds with chat messages in the world
+pub async fn chat_messages<W: WorldState<Message = M>, M: Serialize>(
+    data: web::ThinData<W>,
+) -> impl Responder {
+    let messages = data.get_chat_messages();
+
+    web::Json(messages)
+}
