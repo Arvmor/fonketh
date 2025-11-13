95d1ba5a40c3452e33e9b9d2868f90d1c707252c
Arvin Morawej <arvin.morawej@gmail.com>
2025-11-13 16:23:17 -0500

    chore: Update bootnode IP

--- crates/network/src/p2p/network.rs
+++ crates/network/src/p2p/network.rs
@@ -13,7 +13,7 @@ pub const GAME_PROTO_NAME: StreamProtocol = StreamProtocol::new("/game/kad/1.0.0
 /// Listen address for the peer
 const LISTEN_ADDR: &str = "/ip4/0.0.0.0/udp/7331/quic-v1";
 /// Bootstrap nodes for the peer
-const BOOTSTRAP_NODES: [&str; 1] = ["/ip4/13.220.20.144/udp/7331/quic-v1"];
+const BOOTSTRAP_NODES: [&str; 1] = ["/ip4/44.213.73.150/udp/7331/quic-v1"];
 /// Topics to subscribe to
 const TOPICS: [&str; 1] = ["game_events"];
 
